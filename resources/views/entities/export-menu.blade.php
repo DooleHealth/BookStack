609ccf9ec4528b6b680e698a001fb5c24358d3c4
bpellicer <bpellicer@doolehealth.com>
2026-05-08 13:37:59 +0200

    fix(exportPDF): Styles corrected

--- resources/views/entities/export-menu.blade.php
+++ resources/views/entities/export-menu.blade.php
@@ -13,11 +13,7 @@
     </button>
 
     <ul refs="dropdown@menu" class="wide dropdown-menu" role="menu">
-        {{-- <li><a href="{{ $entity->getUrl('/export/html') }}" target="_blank" role="menuitem" class="label-item"><span>{{ trans('entities.export_html') }}</span><span>.html</span></a></li> --}}
-        <li><a href="{{ $entity->getUrl('/export/pdf') }}" target="_blank" role="menuitem" class="label-item"><span>{{ trans('entities.export_pdf') }}</span><span>.pdf</span></a></li>
-        {{-- <li><a href="{{ $entity->getUrl('/export/plaintext') }}" target="_blank" role="menuitem" class="label-item"><span>{{ trans('entities.export_text') }}</span><span>.txt</span></a></li>
-        <li><a href="{{ $entity->getUrl('/export/markdown') }}" target="_blank" role="menuitem" class="label-item"><span>{{ trans('entities.export_md') }}</span><span>.md</span></a></li>
-        <li><a href="{{ $entity->getUrl('/export/zip') }}" target="_blank" role="menuitem" class="label-item"><span>{{ trans('entities.export_zip') }}</span><span>.zip</span></a></li> --}}
+        <li><a href="{{ $entity->getUrl('/export/pdf-email') }}" role="menuitem" class="label-item"><span>{{ trans('entities.export_pdf') }}</span><span>.pdf</span></a></li>
     </ul>
 
 </div>
